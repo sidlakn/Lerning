4a30d5811f1e93dc9adc916b181307c087334d73
sidlakn <88433110+sidlakn@users.noreply.github.com>
2021-08-12 10:46:46 +0300

    Create 20210812.cs

---
+++ 20210812.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace MyNewApp
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			Nazar.Chrome();
+			Console.WriteLine("Hello Dmutruk");
+		}
+
+		class Nazar
+		{
+			public static void Chrome()
+			{
+				Console.WriteLine("nice code?");
+			}
+		}
+	}
+}
