c453b3f7086dff1ac9e74b5933a07e7926334fea
sidlakn <88433110+sidlakn@users.noreply.github.com>
2021-10-06 15:34:36 +0300

    Create 20211006_variables.cs

---
+++ 20211006_variables.cs
@@ -0,0 +1,20 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int cucumber = 1;
+		bool orange = false;
+		char banana = '2';
+		double peach = 346.3;
+		string coconut = "lemon";
+		Console.WriteLine("Hello");
+		Console.WriteLine(cucumber);
+		Console.WriteLine(orange);
+		Console.WriteLine(banana);
+		Console.WriteLine(peach);
+		Console.WriteLine(coconut);
+	}
+}
+ 
