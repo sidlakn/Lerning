e84951191ad27ca0c97cde83d20498705922641d
sidlakn <88433110+sidlakn@users.noreply.github.com>
2021-10-07 15:40:58 +0300

    Create 20211007_IF.cs

---
+++ 20211007_IF.cs
@@ -0,0 +1,36 @@
+using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		if (1 == 1)
+		{
+			Console.WriteLine("coconut vlad");
+		}
+
+		bool peach = 1 == 1;
+		if (peach)
+		{
+			Console.WriteLine("math1");
+		}
+		else 
+		{
+			Console.WriteLine("math2");
+		}
+		
+		if (2>3)
+		{
+			Console.WriteLine("mouse1");
+		}
+		else if (5>4)
+		{
+			Console.WriteLine("mouse2");
+		}
+		else 
+		{
+			Console.WriteLine("mouse3");
+		}
+         
+	}
+}
