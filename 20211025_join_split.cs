8d222482aad7a1aa711a123cca21c77c1d05973a
sidlakn <88433110+sidlakn@users.noreply.github.com>
2021-10-25 13:33:11 +0300

    Create 20211025_join_split.cs

---
+++ 20211025_join_split.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		string value = "abc123";
+		char[] valueArray = value.ToCharArray();
+		Array.Reverse(valueArray);
+		// string result = new string(valueArray);
+		
+		string result = String.Join(",", valueArray);
+		Console.WriteLine(result);
+		
+		string[] items = result.Split(',');
+		foreach (string item in items)
+		{
+			Console.WriteLine(item);
+		}
+	}
+}
