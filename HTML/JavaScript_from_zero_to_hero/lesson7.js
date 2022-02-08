aa381c45585ff3810437998f5876623de8515ce7
sidlakn <88433110+sidlakn@users.noreply.github.com>
2022-02-09 00:05:19 +0200

    lesson 7

---
+++ HTML/JavaScript_from_zero_to_hero/lesson7.js
@@ -0,0 +1,12 @@
+//var arr = new Array();
+var arr = [5, true, "stroka", 5.7, 0, -100];
+arr[3] = "word";
+//console.log(arr[3]);
+console.log(arr.length);
+
+var matrix = [
+  [4, 6, 8], ["stroka", 5.7], [0, -100]
+];
+
+matrix[1][0] = 90;
+console.log(matrix);
