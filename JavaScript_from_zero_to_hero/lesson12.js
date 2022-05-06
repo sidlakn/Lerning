d1a1f5bba9de766eb7ff9f26079baaa8d71143c5
sidlakn <88433110+sidlakn@users.noreply.github.com>
2022-05-06 14:25:38 +0300

    html

--- JavaScript_from_zero_to_hero/lesson12.js
+++ JavaScript_from_zero_to_hero/lesson12.js
@@ -1,24 +1,24 @@
-// var text = document.getElementById('text');
-// text.title="new text"
-// console.log(text.title);
-//
-// text.style.color ="red";
-// text.style.backgroundColor ="black";
-//
-//
-// text.innerHTML = "New<br>string";
-//
-// document.getElementById('text').style.color = "white";
-//
-// var spans = document.getElementsByTagName('span');
-// for(var i=0; i <spans.length; i++){
-//   console.log(spans[i].innerHTML);
-// }
-//
-// var spans2 = document.getElementsByClassName('simple-text');
-// for(var i=0; i <spans2.length; i++){
-//   console.log(spans2[i].innerHTML);
-// }
+var text = document.getElementById('text');
+text.title="new text"
+console.log(text.title);
+
+text.style.color ="red";
+text.style.backgroundColor ="black";
+
+
+text.innerHTML = "New<br>string";
+
+document.getElementById('text').style.color = "white";
+
+var spans = document.getElementsByTagName('span');
+for(var i=0; i <spans.length; i++){
+  console.log(spans[i].innerHTML);
+}
+
+var spans2 = document.getElementsByClassName('simple-text');
+for(var i=0; i <spans2.length; i++){
+  console.log(spans2[i].innerHTML);
+}
 
 
 function checkForm(el) {
